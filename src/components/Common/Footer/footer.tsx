5f218a209728ff0f35f06d3746e5a28fe8c6e9d6
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-25 17:21:12 +0600

    footer some  content change

--- src/components/Common/Footer/footer.tsx
+++ src/components/Common/Footer/footer.tsx
@@ -76,7 +76,7 @@ export default function Footer() {
             <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
               <div className="mb-10 w-full">
                 <h4 className="text-dark mb-9 text-lg font-semibold">
-                  Company
+               INFORMATION
                 </h4>
                 <ul>
                   <li>
