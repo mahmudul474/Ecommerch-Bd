7e02711ded393b8ef919e30ba748407ad16fae18
mahmudul474 <mdmahmudulla474@gmail.com>
2023-12-18 17:12:31 +0600

    forgate password

--- src/pages/forgatepassword.tsx
+++ src/pages/forgatepassword.tsx
@@ -1,7 +1,9 @@
 import Layout from "@/components/Layots/RootLayot";
 import { useForgatePasswordMutation } from "@/helperConfige/authApi";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { ReactNode, useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Forgatepassword() {
   const [forgatePassword, { isError, isLoading }] =
@@ -16,81 +18,93 @@ export default function Forgatepassword() {
 
     try {
       const res = await forgatePassword({ email }).unwrap();
-      if (res?.data?.accessToken) {
-        router.push("/login");
-      }
-    } catch (error: any) {}
+       
+      toast.success(res.message, { autoClose: 3000 });
+      setTimeout(() => {
+        router.push("/home");
+      }, 3000);
+    } catch (error: any) {
+      toast.error(error.message, { autoClose: 3000 });
+    }
   };
 
   return (
-    <div className="mt-10 ">
-      <div className="max-w-lg mt-32 mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
-        <h1 className="text-4xl font-medium">Reset password</h1>
-        <p className="text-slate-500">Fill up the form to reset the password</p>
+    <>
+      <div className="mt-10 ">
+        <div className="max-w-lg mt-32 mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300">
+          <h1 className="text-4xl font-medium">Reset password</h1>
+          <p className="text-slate-500">
+            Fill up the form to reset the password
+          </p>
 
-        <form action="" className="my-10">
-          <div className="flex flex-col space-y-5">
-            <label htmlFor="email">
-              <p className="font-medium text-slate-700 pb-2">Email address</p>
-              <input
-                onBlur={(e) => {
-                  setEmail(e.target.value);
-                }}
-                id="email"
-                name="email"
-                type="email"
-                className="w-full py-3 border border-slate-200 bg-white rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
-                placeholder="Enter email address"
-              />
-            </label>
-
-            <button className="w-full py-3 font-medium text-black    rounded-lg  border border-black  inline-flex space-x-2 items-center justify-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+          <form onSubmit={sendMailForgatePassword} className="my-10">
+            <div className="flex flex-col space-y-5">
+              <label htmlFor="email">
+                <p className="font-medium text-slate-700 pb-2">Email address</p>
+                <input
+                  onBlur={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  id="email"
+                  name="email"
+                  type="email"
+                  className="w-full py-3 border border-slate-200 bg-white rounded-lg px-3 focus:outline-none focus:border-slate-500 hover:shadow"
+                  placeholder="Enter email address"
                 />
-              </svg>
+              </label>
 
-              <span>Reset password</span>
-            </button>
-            <p className="text-center">
-              Not registered yet?{" "}
-              <a
-                href="#"
-                className="text-indigo-600 font-medium inline-flex space-x-1 items-center"
+              <button
+                type="submit"
+                className="w-full py-3 font-medium text-black    rounded-lg  border border-black  inline-flex space-x-2 items-center justify-center"
               >
-                <span>Register now </span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </span>
-              </a>
-            </p>
-          </div>
-        </form>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+                  />
+                </svg>
+                {isLoading ? <span>Loading...</span> : <span>Reset password</span>}
+              </button>
+              <p className="text-center">
+                Not registered yet?{" "}
+                <a
+                  href="#"
+                  className="text-black  font-medium inline-flex space-x-1 items-center"
+                >
+                  <Link href="/register">
+                    <span>Register now </span>
+                  </Link>
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-4 w-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                      />
+                    </svg>
+                  </span>
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/repassword.tsx
+++ src/pages/repassword.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import Layout from "@/components/Layots/RootLayot";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 export default function Repassword() {
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { token } = router.query;
+
+  const handleNewPassWord = () => {
+    fetch(`https://api.dreamfurniturebd.com/api/v1/auth/reset-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        token: `${token}`,
+      },
+      body: JSON.stringify({ newPassword: password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
-   <></>
-  )
+    <>
+      <div className="flex   my-10 flex-1 flex-col  justify-center space-y-5 max-w-md mx-auto mt-24">
+        <div className="flex flex-col space-y-2 text-center">
+          <h2 className="text-3xl md:text-4xl font-bold">Set New Password</h2>
+        </div>
+        <form
+          onSubmit={handleNewPassWord}
+          className="flex flex-col max-w-md space-y-5"
+        >
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="New Password"
+            className="flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"
+          />
+          <button
+            type="submit"
+            className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-gray-500 text-white"
+          >
+            Confirm
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
+Repassword.getLayout = function getLayout(page: any) {
+  return <Layout>{page}</Layout>;
+};
