c2e0a8c88ab5d8f811c3cfee078e80aa54d4c481
mahmudul474 <mdmahmudulla474@gmail.com>
2023-12-18 15:07:30 +0600

    again check token

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -1,13 +1,15 @@
 import Layout from "@/components/Layots/RootLayot";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Verify() {
   const router = useRouter();
 
   const { token } = router.query;
+  const [loading, setLoading]=useState(false)
 
   useEffect(() => {
+    setLoading(true)
     if (token) {
       fetch(
         `https://api.dreamfurniturebd.com/api/v1/auth/confirm-account?token=${token}`,
@@ -18,11 +20,19 @@ export default function Verify() {
         .then((response) => response.json())
         .then((data) => {
           console.log(data);
+          setLoading(false);
         });
     }
   }, [token]);
 
-  return <div></div>;
+
+
+
+  if(loading){
+    return <h1>Loading....</h1>
+  }
+
+  return <></>;
 }
 Verify.getLayout = function getLayout(page: any) {
   return <Layout>{page}</Layout>;
